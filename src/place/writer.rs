dedff5d9b71f80ebc821d46edca7af86f791c461
Sascha Brawer <sascha@brawer.ch>
2026-01-09 04:35:09 +0100

    Write Parquet output with tags
    On a 6-core MacBook Pro (model 2018), processing the full AllThePlaces
    data took 9 minutes and 47 seconds, with a peak memory footprint of
    272.0 MB, measured with the ATP dump of 2026-01-06T02:33:43Z. The
    resulting Parquet file was 629.9 MB, compared to 1567.5 MB for the
    input ZIP containing GeoJSON.

--- src/place/writer.rs
+++ src/place/writer.rs
@@ -1,7 +1,8 @@
 use super::Place;
 use anyhow::{Ok, Result};
 use arrow::{
-    array::{Int32Array, StringArray, UInt64Array},
+    array::{ArrayRef, Int32Array, MapArray, StringArray, StructArray, UInt64Array},
+    buffer::OffsetBuffer,
     datatypes::{DataType, Field, Schema},
     record_batch::RecordBatch,
 };
@@ -18,6 +19,7 @@ pub struct ParquetWriter {
     schema: Arc<Schema>,
     writer: ArrowWriter<File>,
     places: Vec<Place>,
+    num_tags: usize,
 }
 
 impl ParquetWriter {
@@ -33,6 +35,7 @@ impl ParquetWriter {
             schema,
             writer,
             places: Vec::with_capacity(batch_size),
+            num_tags: 0,
         })
     }
 
@@ -40,6 +43,7 @@ impl ParquetWriter {
         if self.places.len() == self.places.capacity() {
             self.flush()?;
         }
+        self.num_tags += place.tags.len();
         self.places.push(place);
         Ok(())
     }
@@ -62,14 +66,71 @@ impl ParquetWriter {
         let source_urls = Arc::new(StringArray::from_iter(
             self.places.iter().map(|p| p.source_url.as_deref()),
         ));
+        let tags = make_tags(&self.places, self.num_tags);
         let batch = RecordBatch::try_new(
             self.schema.clone(),
-            vec![s2_cell_ids, longitudes, latitudes, source_ids, source_urls],
+            vec![
+                s2_cell_ids,
+                longitudes,
+                latitudes,
+                source_ids,
+                source_urls,
+                tags,
+            ],
         )?;
         self.writer.write(&batch)?;
         self.places.clear();
+        self.num_tags = 0;
         Ok(())
     }
+}
+
+fn make_tags(places: &[Place], num_tags: usize) -> Arc<MapArray> {
+    let mut keys = Vec::with_capacity(num_tags);
+    let mut values = Vec::with_capacity(num_tags);
+    let mut offsets = Vec::with_capacity(num_tags + 1);
+    for place in places.iter() {
+        offsets.push(keys.len() as i32);
+        for (key, value) in place.tags.iter() {
+            keys.push(key.as_ref());
+            values.push(value.as_ref());
+        }
+    }
+    offsets.push(keys.len() as i32);
+
+    let keys = StringArray::from(keys);
+    let values = StringArray::from(values);
+    let offsets = OffsetBuffer::new(offsets.into());
+
+    let struct_array = StructArray::from(vec![
+        (
+            Arc::new(Field::new("key", DataType::Utf8, false)),
+            Arc::new(keys) as ArrayRef,
+        ),
+        (
+            Arc::new(Field::new("value", DataType::Utf8, false)),
+            Arc::new(values) as ArrayRef,
+        ),
+    ]);
+
+    let map_array = MapArray::new(
+        Arc::new(Field::new(
+            "key_value",
+            DataType::Struct(
+                vec![
+                    Field::new("key", DataType::Utf8, false),
+                    Field::new("value", DataType::Utf8, false),
+                ]
+                .into(),
+            ),
+            false, // non-nullable
+        )),
+        offsets,
+        struct_array,
+        None, // no nulls
+        true, // sorted
+    );
+    Arc::new(map_array)
 }
 
 fn make_schema() -> Schema {
@@ -79,22 +140,23 @@ fn make_schema() -> Schema {
         Field::new("latitude_e7", DataType::Int32, /* nullable */ false),
         Field::new("source_id", DataType::Utf8, /* nullable */ false),
         Field::new("source_url", DataType::Utf8, /* nullable */ true),
-        /*
         Field::new(
-                "tags",
-                DataType::Map(
-                    Arc::new(Field::new(
-                        "entries",
-                        DataType::Struct(vec![
+            "tags",
+            DataType::Map(
+                Arc::new(Field::new(
+                    "key_value",
+                    DataType::Struct(
+                        vec![
                             Field::new("key", DataType::Utf8, /* nullable */ false),
                             Field::new("value", DataType::Utf8, /* nullable */ false),
-                        ].into()),
-                        /* nullable */ false,
-                    )),
-                    /* sorted */ true,
-                ),
-                false, // non-nullable
+                        ]
+                        .into(),
+                    ),
+                    /* nullable */ false,
+                )),
+                /* sorted */ true,
+            ),
+            false, // non-nullable
         ),
-        */
     ])
 }

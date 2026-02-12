a5cf8d1195ec44b9ed3468261b97a7db835fdd4b
Sascha Brawer <sascha@brawer.ch>
2026-02-12 11:43:24 +0100

    Rename module `import_osm` to `osm` (#137)

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 mod coverage;
 mod import_atp;
-mod import_osm;
+mod osm;
 mod place;
 mod u64_table;
 
 // Re-exported for main.rs.
 pub use coverage::build_coverage;
 pub use import_atp::import_atp;
-pub use import_osm::import_osm;
+pub use osm::import_osm;
 
 #[cfg(fuzzing)]
 pub use import_atp::fuzz::fuzz_process_geojson;
